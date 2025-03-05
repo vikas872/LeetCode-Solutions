bc6b11bcca2ec7985af59d39aaa4ae226843b1a5
Vikas Pandey <vp295756@gmail.com>
2025-03-05 18:12:13 +0000

    Code challenge submission for question: 116 Populating Next Right Pointers in Each Node

---
+++ 116 Populating Next Right Pointers in Each Node/116populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,24 @@
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution(object):
+    def connect(self, root):
+        if not root:
+            return None
+
+        leftmost = root
+
+        while leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
+        
+        return root
