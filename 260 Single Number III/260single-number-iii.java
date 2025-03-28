da70499772b370c2e6e3ee810c1d37fee5c46803
Vikas Pandey <vp295756@gmail.com>
2025-03-28 15:18:09 +0000

    Code challenge submission for question: 260 Single Number III

---
+++ 260 Single Number III/260single-number-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        
+        int diffBit = xor & (-xor);
+        
+        int num1 = 0, num2 = 0;
+        for (int num : nums) {
+            if ((num & diffBit) == 0) {
+                num1 ^= num;
+            } else {
+                num2 ^= num;
+            }
+        }
+        
+        return new int[]{num1, num2};
+    }
+}
