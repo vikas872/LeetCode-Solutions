20fe28f49dbc369f58d5a40fffb39fce2f3c28b8
Vikas Pandey <vp295756@gmail.com>
2025-04-18 16:10:49 +0000

    Code challenge submission for question: 187 Repeated DNA Sequences

---
+++ 187 Repeated DNA Sequences/187repeated-dna-sequences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String sub = s.substring(i, i + 10);
+            if (!seen.add(sub)) {
+                repeated.add(sub);
+            }
+        }
+        return new ArrayList<>(repeated);
+    }
+}
