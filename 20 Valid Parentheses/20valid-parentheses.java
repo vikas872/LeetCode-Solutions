a5bd6f95def0ecf0ab414316ce1eff3111c0fba7
Vikas Pandey <vp295756@gmail.com>
2025-09-02 16:30:13 +0000

    Code challenge submission for question: 20 Valid Parentheses

---
+++ 20 Valid Parentheses/20valid-parentheses.java
@@ -0,0 +1,22 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
