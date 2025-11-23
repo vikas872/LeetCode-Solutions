4d80aea1527fbcfe6a883c2eff15f67d13ba958c
Vikas Pandey <163318685+vikas872@users.noreply.github.com>
2025-11-24 01:56:44 +0530

    Time: 16 ms (18.81%), Space: 47.2 MB (10.22%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> st = new Stack<>();
+        int n = s.length();
+        int curr = 0;
+        int sign = 1;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                curr = s.charAt(i)-'0';
+                while (i + 1 < n && Character.isDigit(s.charAt(i + 1))) {
+                    curr = curr * 10 + (s.charAt(i + 1) - '0');
+                    i++;
+                }
+                curr = curr*sign;
+                sum+=curr;
+                curr = 0;
+            }else if(s.charAt(i)=='+'){
+                sign = 1;
+            }else if(s.charAt(i)=='-'){
+                sign = -1;
+            }else if(s.charAt(i)=='('){
+                st.push(sum);
+                st.push(sign);
+                sum = 0;
+                sign = 1;
+            }else if(s.charAt(i)==')'){
+                int prvSign = st.pop();
+                sum = sum*prvSign;
+                int prvSum = st.pop();
+                sum = sum + prvSum; 
+            }
+        }
+        return sum;
+    }
+}
