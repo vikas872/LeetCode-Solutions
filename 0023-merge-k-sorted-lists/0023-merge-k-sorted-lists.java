610ac1239b36de7e81d76c2ba23a8ecb66a684b4
Vikas Pandey <163318685+vikas872@users.noreply.github.com>
2026-02-11 11:03:23 +0530

    Time: 5 ms (42.78%), Space: 46.9 MB (43.67%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,46 +1,41 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    class Pair{
+
+    class Pair {
         int val;
         ListNode node;
-        public Pair(Integer val, ListNode node){
+
+        public Pair(int val, ListNode node) {
             this.val = val;
             this.node = node;
         }
     }
+
     public ListNode mergeKLists(ListNode[] arr) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a,b) -> a.val-b.val
-        );
-        for(int i=0;i<arr.length;i++){
-           if(arr[i]!=null){
-             pq.add(new Pair(arr[i].val, arr[i]));
-           }
+
+        PriorityQueue<Pair> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                pq.add(new Pair(arr[i].val, arr[i]));
+            }
         }
+
         ListNode dummy = new ListNode(0);
         ListNode temp = dummy;
 
-        while(pq.isEmpty()==false){
+        while (!pq.isEmpty()) {
             Pair curr = pq.poll();
-            Integer val = curr.val;
             ListNode currNode = curr.node;
+
             temp.next = currNode;
             temp = temp.next;
 
-            if(currNode.next!=null){
+            if (currNode.next != null) {
                 pq.add(new Pair(currNode.next.val, currNode.next));
             }
-            
         }
+
         return dummy.next;
     }
 }
