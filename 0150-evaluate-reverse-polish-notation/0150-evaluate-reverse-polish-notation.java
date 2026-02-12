b44769693485f6bb6179b4c4f4a8f2f65b8d09e1
Vikas Pandey <163318685+vikas872@users.noreply.github.com>
2026-02-12 12:33:09 +0530

    Time: 6 ms (89.85%), Space: 45.6 MB (19.81%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,23 +1,53 @@
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         Stack<Integer> st = new Stack<>();
+//         for (String s : tokens) {
+//             if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+//                 int a = (st.pop());
+//                 int b = (st.pop());
+//                 if (s.equals("+")) {
+//                     st.push( a + b);
+//                 } else if (s.equals("-")) {
+//                     st.push(  b-a);
+//                 } else if (s.equals("*")) {
+//                     st.push( a * b);
+//                 } else {
+//                     st.push( b/a);
+//                 }
+//             } else {
+//                 st.push(Integer.parseInt(s));
+//             }
+//         }
+//         return st.peek();
+//     }
+// }
 class Solution {
-    public int evalRPN(String[] tokens) {
+    public int evalRPN(String[] arr) {
         Stack<Integer> st = new Stack<>();
-        for (String s : tokens) {
-            if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
-                int a = (st.pop());
-                int b = (st.pop());
-                if (s.equals("+")) {
-                    st.push( a + b);
-                } else if (s.equals("-")) {
-                    st.push(  b-a);
-                } else if (s.equals("*")) {
-                    st.push( a * b);
-                } else {
-                    st.push( b/a);
-                }
-            } else {
+        for(String s:arr){
+            if(s.equals("+")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a+b);
+            }
+            else if(s.equals("-")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a-b);
+            }
+            else if(s.equals("*")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a*b);
+            }
+            else if(s.equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a/b);
+            }else{
                 st.push(Integer.parseInt(s));
             }
         }
-        return st.peek();
+        return st.pop();
     }
 }
