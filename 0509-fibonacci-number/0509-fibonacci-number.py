fc6e8585970fc6ee05425f6487dd46734907632a
Vikas Pandey <163318685+vikas872@users.noreply.github.com>
2026-04-09 15:58:45 +0530

    Time: 41 ms (85.53%), Space: 19.4 MB (9.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,12 @@
-class Solution:
-    def f(self,n,dp):
-        if n<=1:
-            return n
-        if dp[n]!=-1:
-            return dp[n]
-        dp[n] = self.f(n-1,dp)+self.f(n-2,dp)
+def f(n,dp):
+    if n<=1:
+        return n
+    if dp[n]!=-1:
         return dp[n]
+    dp[n] = f(n-1,dp)+f(n-2,dp)
+    return dp[n]
+class Solution:
     def fib(self, n: int) -> int:
         dp = [-1]*(n+1)
-        return self.f(n,dp)
+        return f(n,dp)
         
