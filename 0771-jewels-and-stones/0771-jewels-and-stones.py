ae01def57a154ee23a78b0e3831b3784aa6b53ca
Vikas Pandey <163318685+vikas872@users.noreply.github.com>
2026-04-10 14:31:28 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.26%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,13 +1,23 @@
+# class Solution:
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         ans = 0
+#         map = {}
+#         for ch in jewels:
+#             if ch in map:
+#                 map[ch]+=1
+#             else:
+#                 map[ch] = 1
+        
+#         for ch in stones:
+#             ans+=map.get(ch,0)
+#         return ans
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        ans = 0
-        map = {}
-        for ch in jewels:
-            if ch in map:
-                map[ch]+=1
-            else:
-                map[ch] = 1
-        
+        jewel_set = set(jewels)
+        count = 0
+
         for ch in stones:
-            ans+=map.get(ch,0)
-        return ans
+            if ch in jewel_set:
+                count += 1
+
+        return count
