ad5739d5e1a5342988321b4468aef6f04d48bc71
Vikas Pandey <163318685+vikas872@users.noreply.github.com>
2026-05-03 14:56:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.53%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,46 +1,18 @@
-# class Solution:
-#     def nextGreaterElement(self, arr1: List[int], arr2: List[int]) -> List[int]:
-#         n1 = len(arr1)
-#         n2 = len(arr2)
-#         ans = []
-#         for i in range(0,n1):
-#             for j in range(0,n2):
-#                 if arr1[i]==arr2[j]:
-#                     while j<n2-1:
-#                         if arr2[j+1]>arr1[i]:
-#                             ans.append(arr2[j+1])
-#                             break
-#                         j+=1
-                        
-#                     if j==n2-1:
-#                         ans.append(-1)
-#         return ans
-                    
-from typing import List
-
 class Solution:
-    def nextGreaterElement(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        n1 = len(arr1)
-        n2 = len(arr2)
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        st = []
+        mp = {}
+
+        for i in nums2:
+            while st and st[-1]<i:
+                mp[st.pop()] = i
+            st.append(i)
         ans = []
-
-        for i in range(n1):
-            found = False
-
-            for j in range(n2):
-                if arr1[i] == arr2[j]:
-                    
-                    k = j + 1
-                    while k < n2:
-                        if arr2[k] > arr1[i]:
-                            ans.append(arr2[k])
-                            found = True
-                            break
-                        k += 1
-
-                    if not found:
-                        ans.append(-1)
-
-                    break   # IMPORTANT
-
+        ind = 0
+        for i in nums1:
+            if i in mp:
+                ans.append(mp.get(i))
+            else:
+                ans.append(-1)
+            ind+=1
         return ans
